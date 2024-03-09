64cc828ff1a97cb4a68dcb669293ef0654858183
KaitoDeCode <kurniawanadi4556@gmail.com>
2024-03-09 10:57:01 +0700

    feat: Structure folder projek

--- App.jsx
+++ App.jsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Login</Text>
-     
+      <Text>Please login to continue</Text>
+      <TextInput style={styles.input} placeholder="Email" keyboardType="email-address" />
+      <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} />
+      <TouchableOpacity onPress={() => alert('Login belum diimplementasikan')} style={styles.button}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -22,5 +27,35 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     margin: 10,
+  },
+  input:{
+    width: 200,
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#000',
+    borderRadius: 5,
+    padding: 10,
+  },
+  button:{
+    width: 200,
+    height: 40,
+    marginTop: 30,
+    borderWidth: 0,
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: '#FF0000', // Warna merah
+    alignItems: 'center',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  buttonText:{
+    color: '#FFFFFF',
+    fontSize: 16,
+    fontWeight: 'bold',
   }
 });
